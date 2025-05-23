df814b39af848fb951c79166f3f546ab3772b6a2
Isaac <isaacecheverry53@gmail.com>
2025-05-23 10:54:05 -0500

    Creacion de iconos y carpetas

---
+++ pages/Admin/View_Profile.php
@@ -0,0 +1,33 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Profile</title>
+</head>
+<body>
+    <?php 
+        include 
+    ?>
+    <div id="infoAdmin">
+        <h1>Información Administrador</h1>
+        <div id="infoContent">
+            <img src="../../assets/icons/avatar.png" alt="Icono_Usuario">
+            <label>Nombre</label>
+            <input type="text">
+            <div id="infoTitle">
+                
+            </div>
+            <div id="infoTable">
+
+            </div>
+            <div id="editButton">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
