215c41ff5b32649922fabc4ccff9057dc696f476
Luis <lchernandez4474@gmail.com>
2025-05-26 14:28:05 -0500

    Subiendo cambios de login

--- auth/login.php
+++ auth/login.php
@@ -11,47 +11,45 @@
     <title>login page</title>
 </head>
 <body>
-    <div class="container">
-        <div class="container-form">
-        <form class="sign-in" action="../auth/login.php" method="POST">
-            <h2>Iniciar Sesión</h2>
+    <div class="container-form">
+    <form class="sign-in" action="../auth/login.php" method="POST">
+        <h2>Iniciar Sesión</h2>
 
-            <div class="social-networks">
-                <ion-icon name="logo-linkedin"></ion-icon>
-                <ion-icon name="logo-whatsapp"></ion-icon>
-                <ion-icon name="logo-instagram"></ion-icon>
-                <ion-icon name="logo-facebook"></ion-icon>
-            </div>
-            <span>Usar correo y contraseña enviados a su gmail</span>
-
-            <div class="container-select">
-                <select name="tipoDocu" required>
-                    <option value="">Tipo de documento</option>
-                    <option value="TI">Tarjeta de Identidad</option>
-                    <option value="CC">Cédula de Ciudadanía</option>
-                    <option value="Nacionalidad">Nacionalidad</option>
-                    <option value="CE">Cédula de Extranjería</option>
-                </select>
-            </div>
-
-            <div class="container-input">
-                <ion-icon name="mail-unread-outline"></ion-icon>
-                <input type="text" name="email" placeholder="Número de documento">
-            </div>
-
-            <div class="container-input">
-                <ion-icon name="lock-closed-outline"></ion-icon>
-                <input type="password" name="password" placeholder="Password">
-            </div>
-            <button type="submit">Iniciar Sesión</button>
-        </form>
-
+        <div class="social-networks">
+            <ion-icon name="logo-linkedin"></ion-icon>
+            <ion-icon name="logo-whatsapp"></ion-icon>
+            <ion-icon name="logo-instagram"></ion-icon>
+            <ion-icon name="logo-facebook"></ion-icon>
         </div>
-        <div class="container-welcome">
-            <div class="welcome-sign-up welcome">
-                <h3>¡Bienvenido!</h3>
-                <p>Ingrese sus datos personales para usar todas las funciones del sitio </p>
-            </div>
+        <span>Usar correo y contraseña enviados a su gmail</span>
+
+        <div class="container-select">
+            <select name="tipoDocu" required>
+                <option value="">Tipo de documento</option>
+                <option value="TI">Tarjeta de Identidad</option>
+                <option value="CC">Cédula de Ciudadanía</option>
+                <option value="Nacionalidad">Nacionalidad</option>
+                <option value="CE">Cédula de Extranjería</option>
+            </select>
+        </div>
+
+        <div class="container-input">
+            <ion-icon name="mail-unread-outline"></ion-icon>
+            <input type="text" name="email" placeholder="Número de documento">
+        </div>
+
+        <div class="container-input">
+            <ion-icon name="lock-closed-outline"></ion-icon>
+            <input type="password" name="password" placeholder="Password">
+        </div>
+        <button type="submit">Iniciar Sesión</button>
+    </form>
+
+    </div>
+    <div class="container-welcome">
+        <div class="welcome-sign-up welcome">
+            <h3>¡Bienvenido!</h3>
+            <p>Ingrese sus datos personales para usar todas las funciones del sitio </p>
         </div>
     </div>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
